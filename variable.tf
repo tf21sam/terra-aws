192980f3345fb90a914e2cf8e4e662da80be8fbd
Samrat dutta <123928703+tf21sam@users.noreply.github.com>
2025-05-11 23:44:47 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,8 @@
+# define variable for resuability and easy configuration
+variable "bucket_name" {
+    default = "master-ji-ka-bucket-69"
+}
+
+variable "website_index_document" {
+    default = "index.html"
+} 
